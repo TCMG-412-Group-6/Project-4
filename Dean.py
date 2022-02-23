05226b6c92b282a0c90e9fa966fe1445018d1839
Adean64 <andrewjdean64@tamu.edu>
2022-02-23 13:49:16 -0600

    Update Dean.py

--- Dean.py
+++ Dean.py
@@ -35,6 +35,20 @@ files = {}
 sixmonths= 0
 total_count= 0
 months={}
+#these lists hold each line separated by datetime sorting
+log1=[]
+log2=[]
+log3=[]
+log4=[]
+log5=[]
+log6=[]
+log7=[]
+log8=[]
+log9=[]
+log10=[]
+log11=[]
+log12=[]
+log13=[]
 
 print('Parsing log file, this may take a few moments... \n')
 ##this takes a long time to run using regex, I wonder how I can cut it down? 
@@ -60,17 +74,17 @@ for lines in open("log_copy.log", 'r'):
         months[cut_date]+=1
     else:
         months[cut_date]=1
-
+    #adds counts to number of requests by day
     if parsed_date in dates:
         dates[parsed_date]+=1
     else:
         dates[parsed_date]=1
-
+    #adds counts to each filename
     if parsed_filename in files:
         files[parsed_filename]+=1
     else:
         files[parsed_filename]=1
-
+    #adds counts to each error code
     if parsed_code in codes:
         codes[parsed_code]+=1
     else:
@@ -80,64 +94,92 @@ for lines in open("log_copy.log", 'r'):
     startdate = datetime(year=1995, month=4, day=12).date()
     if parsed_date >= startdate:
         sixmonths += 1
+## There absolutely has to be a better way to sort and iterate the monthly logs. This works, but slows down the parse considerably.
+## if parse[0] was capturing the full string, this wouldn't be an issue and I could do it outside of the loop.
+    if datetime(year=1994, month=10, day=1).date() <= parsed_date < datetime(year=1994, month=11, day=1).date():
+        log1.append(lines)
+    if datetime(year=1994, month=11, day=1).date() <= parsed_date < datetime(year=1994, month=12, day=1).date():
+        log2.append(lines)
+    if datetime(year=1994, month=12, day=1).date() <= parsed_date < datetime(year=1995, month=1, day=1).date():
+        log3.append(lines)        
+    if datetime(year=1995, month=1, day=1).date() <= parsed_date < datetime(year=1995, month=2, day=1).date():
+        log4.append(lines)
+    if datetime(year=1995, month=2, day=1).date() <= parsed_date < datetime(year=1995, month=3, day=1).date():
+        log5.append(lines)
+    if datetime(year=1995, month=3, day=1).date() <= parsed_date < datetime(year=1995, month=4, day=1).date():
+        log6.append(lines)
+    if datetime(year=1995, month=4, day=1).date() <= parsed_date < datetime(year=1995, month=5, day=1).date():
+        log7.append(lines)
+    if datetime(year=1995, month=5, day=1).date() <= parsed_date < datetime(year=1995, month=6, day=1).date():
+        log8.append(lines)
+    if datetime(year=1995, month=6, day=1).date() <= parsed_date < datetime(year=1995, month=7, day=1).date():
+        log9.append(lines)
+    if datetime(year=1995, month=7, day=1).date() <= parsed_date < datetime(year=1995, month=8, day=1).date():
+        log10.append(lines)
+    if datetime(year=1995, month=8, day=1).date() <= parsed_date < datetime(year=1995, month=9, day=1).date():
+        log11.append(lines)
+    if datetime(year=1995, month=9, day=1).date() <= parsed_date < datetime(year=1995, month=10, day=1).date():
+        log12.append(lines)
+    if datetime(year=1995, month=10, day=1).date() <= parsed_date < datetime(year=1995, month=11, day=1).date():
+        log13.append(lines)
+
+#repeat protection against re-writing separated log files, this will speed up future executions
+print('Checking if Separate log files are created...\n')
+if not exists('1 Oct 1994.log'):
+    print('Creating Separated Log Files in same Directory as Log Copy...')
+    fh = open('1 Oct 1994.log', 'w')
+    for li in log1:
+        fh.write(f"{li}")
+if not exists('2 Nov 1994.log'):
+    fh = open('2 Nov 1994.log', 'w')
+    for li in log2:
+        fh.write(f"{li}")
+if not exists('3 Dec 1994.log'):
+    fh = open('3 Dec 1994.log','w')
+    for li in log3:
+        fh.write(f"{li}")
+if not exists('4 Jan 1995.log'):
+    fh = open('4 Jan 1995.log','w')
+    for li in log4:
+        fh.write(f"{li}")
+if not exists('5 Feb 1995.log'):
+    fh = open('5 Feb 1995.log','w')
+    for li in log5:
+        fh.write(f"{li}")
+if not exists('6 Mar 1995.log'):
+    fh = open('6 Mar 1995.log','w')
+    for li in log6:
+        fh.write(f"{li}")
+if not exists('7 Apr 1995.log'):
+    fh = open('7 Apr 1995.log','w')
+    for li in log7:
+        fh.write(f"{li}")
+if not exists('8 May 1995.log'):
+    fh = open('8 May 1995.log','w')
+    for li in log8:
+        fh.write(f"{li}")
+if not exists('9 Jun 1995.log'):
+    fh = open('9 Jun 1995.log','w')
+    for li in log9:
+        fh.write(f"{li}")
+if not exists('10 Jul 1995.log'):
+    fh = open('10 Jul 1995.log','w')
+    for li in log10:
+        fh.write(f"{li}")
+if not exists('11 Aug 1995.log'):
+    fh = open('11 Aug 1995.log','w')
+    for li in log11:
+        fh.write(f"{li}")
+if not exists('12 Sep 1995.log'):
+    fh = open('12 Sep 1995.log','w')
+    for li in log12:
+        fh.write(f"{li}")
+if not exists('13 Oct 1995.log'):
+    fh = open('13 Oct 1995.log','w')
+    for li in log13:
+        fh.write(f"{li}")
 
 
-
-## There absolutely has to be a better way to sort and iterate the monthly logs
-## I would ideally like there to be protection against doing this each time the program is executed
-
-    #if datetime(year=1994, month=10, day=1).date() <= parsed_date < datetime(year=1994, month=11, day=1).date():
-        #f = open('1 Oct 1994.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1994, month=11, day=1).date() <= parsed_date < datetime(year=1994, month=12, day=1).date():
-        #f = open('2 Nov 1994.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1994, month=12, day=1).date() <= parsed_date < datetime(year=1995, month=1, day=1).date():
-        #f = open('3 Dec 1994.log', 'w')
-        #f.write(lines,'\n')        
-
-    #if datetime(year=1995, month=1, day=1).date() <= parsed_date < datetime(year=1995, month=2, day=1).date():
-        #f = open('4 Jan log.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=2, day=1).date() <= parsed_date < datetime(year=1995, month=3, day=1).date():
-        #f = open('5 Feb log.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=3, day=1).date() <= parsed_date < datetime(year=1995, month=4, day=1).date():
-        #f = open('6 Mar log.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=4, day=1).date() <= parsed_date < datetime(year=1995, month=5, day=1).date():
-        #f = open('7 Apr log.log', 'w')
-         #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=5, day=1).date() <= parsed_date < datetime(year=1995, month=6, day=1).date():
-        #f = open('8 May log.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=6, day=1).date() <= parsed_date < datetime(year=1995, month=7, day=1).date():
-        #f = open('9 Jun 1995 log.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=7, day=1).date() <= parsed_date < datetime(year=1995, month=8, day=1).date():
-        #f = open('10 Jul log.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=8, day=1).date() <= parsed_date < datetime(year=1995, month=9, day=1).date():
-        #f = open('11 Aug 1995 log.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=9, day=1).date() <= parsed_date < datetime(year=1995, month=10, day=1).date():
-        #f = open('12 Sep 1995 log.log', 'w')
-        #f.write(lines,'\n')
-
-    #if datetime(year=1995, month=10, day=1).date() <= parsed_date < datetime(year=1995, month=11, day=1).date():
-        #f = open('13 Oct log.log', 'w')
-        #f.write(lines,'\n')
-    
 top_file = max(files, key=files.get)
 top_file_value = max(files.values())
 bottom_file = min(files, key=files.get)
