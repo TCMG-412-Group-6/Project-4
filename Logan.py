15ab884672cf3df87c385f3a9703761141915277
Logan-MacDonald <logan.macdonald@tamu.edu>
2022-02-23 14:08:37 -0600

    edit to create count, error count

--- Logan.py
+++ Logan.py
@@ -5,12 +5,17 @@
 
 from ast import Str
 import re
+import datetime
+import os
+from collections import Counter
 # use os.path for file checking capability to check if log file has already been downloaded
 from os.path import exists
 import string
 from tokenize import String
 # use urlretrieve to retrieve log file after file check determined log had not already been downloaded
 from urllib.request import urlretrieve
+
+
 
 FILE_NAME = 'http_access_log.txt'
 
@@ -33,11 +38,14 @@ unsuccessful_requests_404 =0
 unsuccessful_requests_4 =0
 redirected_requests = 0
 past_year = '/1995'
-days = {}
+days = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0, "Friday": 0, "Saturday": 0, "Sunday": 0}
 weeks = {}
 months = {}
 filenames = {}
+files = {}
 
+previous_month_file = ""
+month_file = ""
 
 with open("http_access_log.txt") as fh:
     Lines = fh.readlines()
@@ -59,10 +67,31 @@ with open("http_access_log.txt") as fh:
             if "302 -" in line:
                 redirected_requests += 1
         #Use RegEx to split the line and sort the data in the log
-        #result = re.split('.+ \[(.+) .+\] "[A-Z]{3,5} (.+) HTTP/1.0" ([0-9]{3})', line)
-        
+        result = re.split('.+ \[(.+) .+\] "[A-Z]{3,5} (.+) HTTP/1.0" ([0-9]{3})', line)
+         # make sure the line is a proper request
+        if len(result) == 5:
+            date = result[1]
+            file = result[2]
             
-        
+            # split the date and time, sort dates into dictionary
+            date = date.split(':')
+            if date[0] in days:
+                days[date[0]] += 1
+            else:
+                days[date[0]] = 1
+            
+            # split day and month, sort months into dictionary    
+            date[0] = date[0].split('/')
+            if date[0][1] + " " + date[0][2] in months:
+                months[date[0][1] + " " + date[0][2]] += 1
+            else:
+                months[date[0][1] + " " + date[0][2]] = 1
+            
+            # sort file names into dictionary
+            if file in filenames:
+                filenames[file] += 1
+            else:
+                filenames[file] = 1
         
 
 print("total requests", total_requests)
@@ -71,3 +100,10 @@ print("403", unsuccessful_requests_403)
 print("404", unsuccessful_requests_404)
 print("unsuccessful", unsuccessful_requests)
 print("redirected", redirected_requests)
+
+# printing the results
+print("------------- Results -------------\n")
+# number of past year and total requests
+print("-----------------------------------")       
+print("Total requests in the last year: " + str(past_year_requests))
+print("Total requests in entire log: " + str(total_requests)iii)
