8d965f4886acf8eb38731b79f6089c45bb5050df
astrojoya09 <98620067+astrojoya09@users.noreply.github.com>
2022-02-22 21:43:10 -0600

    Update to parser
    I  am still working on finding most and least requested files and the amount of requests per week.

--- p4_parser.py
+++ p4_parser.py
@@ -16,12 +16,15 @@ if FileExists == False:
 
 else:
     print ("File already exists")
-
+#File should already be downloded
 # Opens file
 log_file = open("log_file.txt", 'r')
 
-#conter for requests
+#couter for requests
 total_count = 0
+redirection_codes = 0
+client_errors = 0
+
 
 months = {
     1: 0,
@@ -53,7 +56,7 @@ days = {
 regex = re.compile("(.*?) - - \[(.*?):(.*) .*\] \"[A-Z]{3,6} (.*?) HTTP.*\" (\d{3}) (.+)")
 
 for line in log_file:
-    total_count = total_count = total_count + 1
+    total_count = total_count + 1
 
     sections = regex.split(log_file)
 
@@ -67,29 +70,29 @@ for line in log_file:
 # I do not know if I should use isoweekday or just weekday
     days[datestamp.weekday] += 1
     print(datestamp.weekday)
+
+    #Get codes for redirection and client errors
     
-    
-print(months)
-print(days)
+    if re.search(sections[6]) == "3**":
+        redirection_codes =+ 1
+    if re.search(sections[6]) == "4**":
+        client_errors =+ 1
+
+   # Might delete if it messes things up
+# trying to integrate some of Dean's code with mine
+#requests = log_file.read()
+#numlines = requests.split("\n")
+#for i in numlines:
+ #   if i:
+  #      total_count += 1 
+
+percent_redirected = int(redirection_codes)/int(total_count)*100
+percent_client_errors = int(client_errors)/int(total_count)*100
+        
+print("The total amount of requests: ", total_count)
+print("The total requests for each month : /n", months)
+print("The total requests for each day of the week: /n", days)
+print("The percentage of redirected requests: ", percent_redirected)
+print("The percentage of requests that led to client error: ", percent_client_errors)
 
 log_file.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
